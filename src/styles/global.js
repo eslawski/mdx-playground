20d85f038b30b0659c94a0d859f4883b2b3fd416
Evan Slawski <evan.slawski@ithaka.org>
2019-09-01 14:14:21 -0400

    Start implementing blog 15

--- src/styles/global.js
+++ src/styles/global.js
@@ -17,7 +17,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
   
-  p {
+  p, li, ul {
     color: rgba(0, 0, 0, .8);
   }
 
@@ -44,6 +44,26 @@ header div a {
   &:hover {
     color: black !important;
   }
+}
+
+blockquote {
+  background: #f9f9f9;
+  border-left: 10px solid #ccc;
+  margin: 1.5em 10px;
+  padding: 0.5em 10px;
+  quotes: "\\201C""\\201D""\\2018""\\2019";
+}
+blockquote:before {
+  color: #ccc;
+  content: open-quote;
+  font-size: 4em;
+  line-height: 0.1em;
+  margin-right: 0.25em;
+  vertical-align: -0.4em;
+}
+blockquote p {
+  display: inline;
+  font-style: italic;
 }
   
 `
